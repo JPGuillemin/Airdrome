eac1b14c214fa1f5c1ad761fdb227dd2d9ef10a5
Hyperion <hyp3ri0n@sfr.fr>
2025-06-08 10:46:59 +0200

    Reorder Discover page

--- src/discover/Discover.vue
+++ src/discover/Discover.vue
@@ -1,23 +1,17 @@
 <template>
   <ContentLoader :loading="loading">
-    <div v-for="section in sections" :key="section.key" class="mb-4">
-      <template v-if="result[section.key].length > 0">
-        <h3>
-          {{ section.name }}
-          <router-link
-            :to="{name: 'albums', params: {sort: section.key}}"
-            class="text-muted"
-          >
-            <Icon icon="chevron-right" />
-          </router-link>
-        </h3>
-        <AlbumList :items="result[section.key]" allow-h-scroll />
-      </template>
+    <div v-if="result.added.length > 0" class="mb-4">
+      <h3>
+        Recently Added
+        <router-link :to="{name: 'albums', params: {sort: 'added'}}" class="text-muted">
+          <Icon icon="chevron-right" />
+        </router-link>
+      </h3>
+      <AlbumList :items="result.added" allow-h-scroll />
     </div>
-
     <div v-if="result.genres.length > 0" class="mb-4">
       <h3>Genres</h3>
-      <div class="d-flex flex-wrap gap-3">
+      <div class="d-flex flex-wrap gap-2">
         <span v-for="item in result.genres" :key="item.id" class="text-bg-secondary rounded-pill py-2 px-3 +mb-2 +me-2">
           <router-link :to="{name: 'genre', params: { id: item.id } }">
             {{ item.name }}
@@ -25,7 +19,6 @@
         </span>
       </div>
     </div>
-
     <div v-if="result.random.length > 0" class="mb-4">
       <h3>
         Random
@@ -35,7 +28,24 @@
       </h3>
       <AlbumList :items="result.random" allow-h-scroll />
     </div>
-
+    <div v-if="result.played.length > 0" class="mb-4">
+      <h3>
+        Recently Played
+        <router-link :to="{name: 'albums', params: {sort: 'played'}}" class="text-muted">
+          <Icon icon="chevron-right" />
+        </router-link>
+      </h3>
+      <AlbumList :items="result.played" allow-h-scroll />
+    </div>
+    <div v-if="result.most.length > 0" class="mb-4">
+      <h3>
+        Most Played
+        <router-link :to="{name: 'albums', params: {sort: 'most'}}" class="text-muted">
+          <Icon icon="chevron-right" />
+        </router-link>
+      </h3>
+      <AlbumList :items="result.most" allow-h-scroll />
+    </div>
     <EmptyIndicator v-if="empty" />
   </ContentLoader>
 </template>
@@ -53,42 +63,35 @@
       return {
         loading: true as boolean,
         result: {
-          'recently-added': [] as Album[],
-          'recently-played': [] as Album[],
-          'most-played': [] as Album[],
+          added: [] as Album[],
+          played: [] as Album[],
+          most: [] as Album[],
           random: [] as Album[],
           genres: [] as Genre[],
         }
       }
     },
     computed: {
-      sections(): any[] {
-        return [
-          { name: 'Recently added', key: 'recently-added' },
-          { name: 'Recently played', key: 'recently-played' },
-          { name: 'Most played', key: 'most-played' },
-        ]
-      },
       empty() {
         return Object.values(this.result).findIndex(x => x.length > 0) === -1
       }
     },
     created() {
-      this.$api.getAlbums('recently-added', 30).then(result => {
-        this.result['recently-added'] = result
+      this.$api.getAlbums('recently-added', 15).then(result => {
+        this.result.added = result
         this.loading = false
       })
       this.$api.getAlbums('recently-played', 15).then(result => {
-        this.result['recently-played'] = result
+        this.result.played = result
       })
       this.$api.getAlbums('most-played', 20).then(result => {
-        this.result['most-played'] = result
+        this.result.most = result
       })
-      this.$api.getAlbums('random', 50).then(result => {
+      this.$api.getAlbums('random', 25).then(result => {
         this.result.random = result
       })
       this.$api.getGenres().then(result => {
-        this.result.genres = orderBy(result, 'albumCount', 'desc')
+        this.result.genres = orderBy(result, 'name', 'asc')
       })
     }
   })
