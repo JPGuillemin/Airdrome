01e6bb3db6e0ac3543d668c16db15bfdee77ee66
Hyperion <hyp3ri0n@sfr.fr>
2026-01-02 15:11:52 +0100

    put mood playlist back

--- src/discover/Discover.vue
+++ src/discover/Discover.vue
@@ -34,14 +34,14 @@
       <AlbumList :items="result.recent" tile-size="100" allow-h-scroll />
     </div>
 
-    <div v-if="result.played.length > 0" class="section-wrapper">
-      <router-link :to="{ name: 'albums', params: { sort: 'recently-played' } }" class="d-inline-flex align-items-center">
-        <Icon icon="recent" class="title-color me-2" />
+    <div v-if="result.mood.length > 0" class="section-wrapper">
+      <router-link :to="{ name: 'genre', params: { id: lastGenre.name } }" class="d-inline-flex align-items-center">
+        <Icon icon="genres" class="title-color me-2" />
         <span class="section-title">
-          Recently played
+          Current mood
         </span>
       </router-link>
-      <AlbumList :items="result.played" tile-size="60" allow-h-scroll title-only />
+      <AlbumList :items="result.mood" tile-size="60" allow-h-scroll title-only />
     </div>
 
     <div v-if="result.favartists.length > 0" class="section-wrapper">
@@ -64,14 +64,14 @@
       <AlbumList :items="result.favalbums" tile-size="100" allow-h-scroll />
     </div>
 
-    <div v-if="result.random.length > 0" class="section-wrapper">
-      <router-link :to="{ name: 'albums', params: { sort: 'random' } }" class="d-inline-flex align-items-center">
-        <Icon icon="random" class="title-color me-2" />
+    <div v-if="result.played.length > 0" class="section-wrapper">
+      <router-link :to="{ name: 'albums', params: { sort: 'recently-played' } }" class="d-inline-flex align-items-center">
+        <Icon icon="recent" class="title-color me-2" />
         <span class="section-title">
-          Random
+          Recently played
         </span>
       </router-link>
-      <AlbumList :items="result.random" tile-size="60" allow-h-scroll title-only twin-rows />
+      <AlbumList :items="result.played" tile-size="60" allow-h-scroll title-only />
     </div>
 
     <div v-if="result.recent.length > 0" class="section-wrapper">
@@ -91,7 +91,7 @@
   import AlbumList from '@/library/album/AlbumList.vue'
   import PlaylistList from '@/library/playlist/PlaylistList.vue'
   import ArtistList from '@/library/artist/ArtistList.vue'
-  import { Album, Genre, Artist, Playlist } from '@/shared/api'
+  import { Album, AlbumGenre, Genre, Artist, Playlist } from '@/shared/api'
   import { orderBy } from 'lodash-es'
 
   export default defineComponent({
@@ -108,14 +108,15 @@
       const result = ref({
         recent: [] as Album[],
         played: [] as Album[],
-        random: [] as Album[],
+        mood: [] as Album[],
         most: [] as Album[],
         favalbums: [] as Album[],
         favartists: [] as Artist[],
         genres: [] as Genre[],
         playlists: [] as Playlist[],
-        inthemood: [] as Album[],
       })
+
+      const lastGenre = ref<AlbumGenre | null>(null)
 
       const empty = computed(() =>
         Object.values(result.value).findIndex(x => x.length > 0) === -1
@@ -124,6 +125,7 @@
       const fetchData = async() => {
         if (loading.value) return
         loading.value = true
+
         try {
           api.getGenres().then((genres: Genre[]) => {
             const genreNames = genres.map((genre: Genre) => ({ ...genre, id: genre.name }))
@@ -137,17 +139,20 @@
             result.value.recent = recent
           })
 
-          api.getAlbums('recently-played', 32).then(played => {
+          api.getAlbums('recently-played', 32).then(async played => {
             result.value.played = played
+            if (played.length === 0) return
+
+            const lastPlayed = played[0]
+            lastGenre.value = (lastPlayed as Album).genres[0]!
+            if (!lastGenre.value) return
+            const albumsByGenre = await api.getAlbumsByGenre(lastGenre.value.name, 32)
+            result.value.mood = albumsByGenre
           })
 
           api.getFavourites().then(favourites => {
             result.value.favartists = favourites.artists.slice(0, 16)
             result.value.favalbums = favourites.albums.slice(0, 16)
-          })
-
-          api.getAlbums('random', 32).then(random => {
-            result.value.random = random
           })
 
           api.getAlbums('most-played', 32).then(most => {
@@ -162,6 +167,7 @@
 
       return {
         result,
+        lastGenre,
         loading,
         empty,
         fetchData,
