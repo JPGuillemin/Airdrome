f96bac3c0dddb557159c727260724ee6967901f8
Hyperion <hyp3ri0n@sfr.fr>
2026-05-07 20:47:12 +0200

    changed font to Roboto

--- src/shared/components/IconReplayGain.vue
+++ src/shared/components/IconReplayGain.vue
@@ -4,6 +4,6 @@
        width="1em" height="1em" fill="currentColor"
        viewBox="0 0 24 24"
        class="icon bi">
-    <text x="0" y="50%" dominant-baseline="central" text-anchor="start" font-family="Arial, sans-serif" font-weight="bold" font-size="16" fill="currentColor">RG</text>
+    <text x="0" y="50%" dominant-baseline="central" text-anchor="start" font-family="Roboto, sans-serif" font-weight="bold" font-size="16" fill="currentColor">RG</text>
   </svg>
 </template>
