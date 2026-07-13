d95d3ee4e146dbc23be876827c85d41f6bd3cf33
Hyperion <hyp3ri0n@sfr.fr>
2026-07-13 15:22:28 +0200

    request ignore doze

--- android/app/src/main/java/com/jpguillemin/airdrome/MainActivity.java
+++ android/app/src/main/java/com/jpguillemin/airdrome/MainActivity.java
@@ -1,34 +1,49 @@
 package com.jpguillemin.airdrome;
 
+import android.net.Uri;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.webkit.WebView;
-
+import android.os.PowerManager;
+import android.provider.Settings;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        registerPlugin(MediaSessionPlugin.class);
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    registerPlugin(MediaSessionPlugin.class);
 
-        super.onCreate(savedInstanceState);
+    super.onCreate(savedInstanceState);
 
-        WebView webView = this.bridge.getWebView();
+    WebView webView = this.bridge.getWebView();
+    requestIgnoreBatteryOptimizations();
 
-        // Disable Android native long press behavior
-        webView.setLongClickable(false);
-        webView.setHapticFeedbackEnabled(false);
-        webView.setOnLongClickListener(v -> true);
+    // Disable Android native long press behavior
+    webView.setLongClickable(false);
+    webView.setHapticFeedbackEnabled(false);
+    webView.setOnLongClickListener(v -> true);
 
-        // Start media service
-        Intent svc = new Intent(this, MediaPlaybackService.class);
+    // Start media service
+    Intent svc = new Intent(this, MediaPlaybackService.class);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            startForegroundService(svc);
-        } else {
-            startService(svc);
-        }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      startForegroundService(svc);
+    } else {
+      startService(svc);
     }
+  }
+
+  public void requestIgnoreBatteryOptimizations() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
+
+      if (!pm.isIgnoringBatteryOptimizations(getPackageName())) {
+        Intent intent = new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+        intent.setData(Uri.parse("package:" + getPackageName()));
+        startActivity(intent);
+      }
+    }
+  }
 }
